7df2913c00bcc900ea829fdec120408df75e9c1a
Richard <hausman.richard@gmail.com>
2021-01-03 09:22:25 -0500

    Correctly imported React Bootstrap styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import logo from './Assets/logo.svg';
+import { Jumbotron, Button, Row, Col } from "react-bootstrap";
 import './Styles/App.css';
 
 // custom components
 import FileForm from './Components/FileForm'
 
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <FileForm />
-      </header>
-
-    </div>
+      <Row>
+        <Col>
+          <Jumbotron style={{ background: "rgba(62, 224, 191, 0.8)" }}>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+          </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron >
+        </Col>
+        <Col>
+          <Jumbotron style={{ background: "rgba(62, 224, 191, 0.8)" }}>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+          </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron >
+        </Col>
+      </Row>
+    </div >
   );
 }
 
--- src/Components/FileForm.js
+++ src/Components/FileForm.js
@@ -1,23 +1,35 @@
+import { useEffect, useState } from "react"
 import { Form, Jumbotron } from "react-bootstrap"
 
-function FileForm(props) {
-    const { stylesList = ["starry Night", "escher"] } = props;
+function FileForm() {
+    //const { stylesList = ["starry Night", "escher"] } = props;
+    const [hasError, setErrors] = useState(false);
+    const [stylesList, setStylesList] = useState([]);
+    //on load, fetch API data
+    useEffect(() => { getAvailableStyles(setStylesList, setErrors) }, [])
+
     return (
         <Jumbotron >
             <Form>
                 <Form.Group id="styleFormGroup" controlId="inputForm.style">
                     <Form.Label>Choose a Style!</Form.Label>
                     <Form.Control as="select">
-                        {stylesList.map((s) => <option>{s}</option>)}
+                        {stylesList.map((s) => <option key={s}>{s}</option>)}
                     </Form.Control>
                 </Form.Group>
                 <Form.Group id="fileInputGroup" controlId="inputForm.file">
                     <Form.File id="inputFile" label="Upload Image" />
                 </Form.Group>
             </Form>
-        </Jumbotron>
+        </ Jumbotron>
     )
 }
 
+async function getAvailableStyles(setter, errorSetter) {
+    const ls = await fetch("http://localhost:8000/test_response")
+    ls.json()
+        .then(ls => setter(ls))
+        .catch(err => errorSetter(err))
+}
 
 export default FileForm
